76a5fdc8cc1cdd9b93ec9af789c48b0525894d80
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-15 08:36:16 +0700

    navbar and assets added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      {/* Upper Navbar */}
+      <div>
+        <div>
+          <div>
+            <a href="#"></a>
+            <img src={logo} alt="logo" />
+          </div>
+          {/* search bar and order button */}
+          <div>
+            <div>
+              <input type="text" placeholder="Search" className=' ' />
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
   )
 }
 
