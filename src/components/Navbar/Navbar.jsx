9775e992ebf47a63da559fff936d8fe958cbe866
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-15 10:16:39 +0700

    darkmode component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "../../assets/logo.png";
 import { IoMdSearch } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6";
+import DarkMode from "./DarkMode";
 
 const Navbar = () => {
   return (
@@ -16,7 +17,7 @@ const Navbar = () => {
             </a>
           </div>
           {/* search bar*/}
-          <div>
+          <div className="flex justify-between items-center gap-4">
             <div className="relative group hidden sm:block">
               <input
                 type="text"
@@ -25,17 +26,22 @@ const Navbar = () => {
               />
               <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
             </div>
+            {/* order button */}
+            <button
+              onClick={() => alert("Ordering not available yet")}
+              className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group"
+            >
+              <span className="group-hover:block hidden transition-all duration-200">
+                Order
+              </span>
+              <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
+            </button>
+
+            {/* Dark Mode Switch */}
+            <div>
+              <DarkMode />
+            </div>
           </div>
-          {/* order button */}
-          <button
-            onClick={() => alert("Ordering not available yet")}
-            className="bg-gradient-to-r from-primary to-secondary transition-all duration-200 text-white py-1 px-4 rounded-full flex items-center gap-3 group"
-          >
-            <span className="group-hover:block hidden transition-all duration-200">
-              Order
-            </span>
-            <FaCartShopping />
-          </button>
         </div>
       </div>
       {/* lower navbar */}
