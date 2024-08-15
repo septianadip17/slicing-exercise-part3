44d7bbb0ffeb3fa37f8da105ed088ff9b21bc2b4
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-15 08:07:58 +0700

    tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div
+      className="text-5xl text-bold text-red-500 bg-slate-800"
+    >
+      App
+    </div>
+  );
+};
 
-export default App
+export default App;
