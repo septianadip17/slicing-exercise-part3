48f37a707b6b582c92a9e99da1390645fe63421e
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-18 11:45:40 +0700

    image list

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import image1 from "../../assets/hero/women.png";
+import image2 from "../../assets/hero/shopping.png";
+import image3 from "../../assets/hero/sale.png";
 
+const imageList = [
+  {
+    id: 1,
+    img: image1,
+    title: "Up to 50% off on all Men's Wear",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolorem soluta facere, mollitia quasi maiores fuga ipsam sed molestiae nostrum!",
+  },
+  {
+    id: 2,
+    img: image2,
+    title: "Up to 50% off on all Men's Wear",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolorem soluta facere, mollitia quasi maiores fuga ipsam sed molestiae nostrum!",
+  },
+  {
+    id: "3",
+    img: "image3",
+    title: "Up to 50% off on all Men's Wear",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolorem soluta facere, mollitia quasi maiores fuga ipsam sed molestiae nostrum!",
+  },
+];
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+  return <div>Hero</div>;
+};
 
-export default Hero
+export default Hero;
