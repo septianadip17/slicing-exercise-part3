a23dba899f86caca84d9108cb2d3aa13237ad43d
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-20 10:32:25 +0700

    react slick installed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import image1 from "../../assets/hero/women.png";
 import image2 from "../../assets/hero/shopping.png";
 import image3 from "../../assets/hero/sale.png";
+import Slider from "react-slick";
 
 const imageList = [
   {
@@ -30,13 +31,16 @@ const Hero = () => {
   return (
     <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center  dark:bg-gray-950 dark:text-white duration-200">
       {/* background pattern */}
-      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9"></div>
+      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-[8]"></div>
       {/* hero section */}
       <div className="container pb-8 sm:pb-0"> 
+        <Slider>
+          
+        </Slider>
         <div>
           <div className="grid grid-cols-1 sm:grid-cols-2">
             {/* text content section */}
-            <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm-order-1">
+            <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm-order-1 relative bg-zinc-100">
               <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">Lorem ipsum dolor sit amet.</h1>
               <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam animi optio, architecto iste maiores reprehenderit ut suscipit nostrum aperiam mollitia.</p>
               <div>
@@ -44,9 +48,9 @@ const Hero = () => {
               </div>
             </div>
             {/* image section */}
-            <div>
-              <div>
-                <img src="" alt="" />
+            <div className="order-1 sm-order-2">
+              <div className="relative z-10">
+                <img src={image1} alt="image 1" className="w-[300px] h-[300px] sm:h-[450px] sm:w-[450px] sm:scale-125 object-contain mx-auto " />
               </div>
             </div>
 
