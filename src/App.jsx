87852e788e498a2f39c37a516b05f8b0831000ec
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-20 13:23:19 +0700

    aos imported

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Product from "./components/Products/Product";
+import AOS from "aos";
 
 const App = () => {
+  React.useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
   return (
     <div>
       <Navbar />
