b1c7203d3e3017b666013207e3fd9cd2cee91681
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-20 10:22:25 +0700

    hero header

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -14,20 +14,47 @@ const imageList = [
   {
     id: 2,
     img: image2,
-    title: "Up to 50% off on all Men's Wear",
+    title: "Up to 60% off on all Men's Wear",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolorem soluta facere, mollitia quasi maiores fuga ipsam sed molestiae nostrum!",
   },
   {
     id: "3",
     img: "image3",
-    title: "Up to 50% off on all Men's Wear",
+    title: "Up to 70% off on all Men's Wear",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolorem soluta facere, mollitia quasi maiores fuga ipsam sed molestiae nostrum!",
   },
 ];
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center  dark:bg-gray-950 dark:text-white duration-200">
+      {/* background pattern */}
+      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9"></div>
+      {/* hero section */}
+      <div className="container pb-8 sm:pb-0"> 
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2">
+            {/* text content section */}
+            <div className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm-order-1">
+              <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">Lorem ipsum dolor sit amet.</h1>
+              <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam animi optio, architecto iste maiores reprehenderit ut suscipit nostrum aperiam mollitia.</p>
+              <div>
+                <button className="bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full">Order Now</button>
+              </div>
+            </div>
+            {/* image section */}
+            <div>
+              <div>
+                <img src="" alt="" />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
